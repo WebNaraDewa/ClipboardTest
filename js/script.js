308caa5e4298e0cdb570bc07ab902618fb13416b
Tomoaki Higa <tomoaki.higa@web-naradewa.com>
2023-12-30 18:07:26 +0900

    対応不可のブラウザに対してボタン非活性化と文言表示

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
 const btn = document.getElementById('btn');
 const img = document.getElementById('img');
 
+document.addEventListener('DOMContentLoaded', function() {
+  
+    // Safariを使用している場合はボタンを非活性にする
+    if (isSafariOrFirefox()) {
+        btn.disabled = true;
+  
+    // メッセージを表示するテキスト要素を作成
+    let message = document.createElement('p');
+    message.textContent = 'このブラウザでは直接画像をクリップボードに保存できません。代わりに、画像を選択してコピーしてください。';
+      
+    // メッセージをボタンの後に追加
+    btn.parentNode.insertBefore(message, btn.nextSibling);
+    }
+  });
+
+
+
 btn.addEventListener('click', async()=>{
     try{
         // canvas要素作成
@@ -23,3 +40,9 @@ btn.addEventListener('click', async()=>{
         alert('クリップボードの保存に失敗しました。(エラー)' + err);        
     }
 })
+
+// SafariまたはFirefoxを使用しているかどうかを判定する関数
+function isSafariOrFirefox() {
+    var userAgent = navigator.userAgent.toLowerCase();
+    return userAgent.includes("safari") && !userAgent.includes("chrome") || userAgent.includes("firefox");
+}
